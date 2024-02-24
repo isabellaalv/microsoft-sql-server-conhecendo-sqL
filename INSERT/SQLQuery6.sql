7446af4bbbb67ff439acc69d42570add64b75506
isabellaalv <issa.boosilva@gmail.com>
2024-02-24 16:21:00 -0300

    INSERT

---
+++ INSERT/SQLQuery6.sql
@@ -0,0 +1,16 @@
+INSERT INTO [TBL_PRODITOS]
+VALUES ('1040107', 'Light - 350ml Melancia', 'Lata', '350ml', 'Melancia', 4.56),
+('26363', 'Energético - 600ml Manga', 'Lata','600 ml', 'Manga', 10.50 ),
+('908727', '2L - Guarana', 'Garrafa', '2L', 'Guarana', 9.60),
+('28902', '1L - Uva', 'Garrafa', '1L', 'Uva', 9.00),
+('579002', '600ml - Abacaxi', 'Garrafa', '600ml', 'Abacaxi', 6.00);
+
+INSERT INTO [TBL_CLIENTES]
+VALUES ('67843208632', 'João Pedro', 'Av Bom Pastor', '236', 'São Judas', 'SP', '19865402',
+'1995-12-20', 57, 'M', 1600, 3000.30, 1), ('43910620640', 'Maria Isabel', 'Rua 23 de Maio', '780', 'Vila Nova', 
+'SP', '68032176', '1999-08-16', 24, 'M', 2800, 4000.30, 0);
+
+INSERT INTO [TBL_VENDEDOR]
+VALUES ('00236', 'Carlos da Silva', 00.5), 
+('00237', 'Amanda Soares', 00.5),
+('00238', 'Pedro Souza', 00.5);
